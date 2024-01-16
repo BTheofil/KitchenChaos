34dd0606969f07c9e99819d007220195d02b1b47
BTheofil <btheofil7@gmail.com>
2024-01-16 20:26:04 +0100

    add new containercounter
    add animation, player can grab, refactor

--- Assets/Script/ClearCounter.cs
+++ Assets/Script/ClearCounter.cs
@@ -2,16 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ClearCounter : MonoBehaviour {
+public class ClearCounter : BaseCounter {
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
-    [SerializeField] private Transform counterTopPoint;
 
-    public void Interact() {
-        Debug.Log("Interact");
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
-
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+    public override void Interact(Player player) {
+        
     }
 }
