7f96453310e28dec48013569e7e391662a7102eb
BTheofil <btheofil7@gmail.com>
2024-03-10 12:21:06 +0100

    sound effects

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -7,6 +7,8 @@ public class DeliveryManager : MonoBehaviour {
 
     public event EventHandler OnRecipeSpawned;
     public event EventHandler OnRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFailed;
     public static DeliveryManager Instance { get; private set; }
 
     [SerializeField] private RecipeListSO recipeListSO;
@@ -70,6 +72,7 @@ public class DeliveryManager : MonoBehaviour {
                     waitingRecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
                     return;
                 }
 
@@ -78,6 +81,7 @@ public class DeliveryManager : MonoBehaviour {
 
         //no matches found
         Debug.Log("No matches");
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
     }
 
     public List<RecipeSO> GetWaitingRecipeSOList() {
